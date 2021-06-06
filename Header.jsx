fb373d0653c0c6c157a287218103186c76d1d9f6
Tharun G <65439029+TharunNo1@users.noreply.github.com>
2021-06-06 09:48:45 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1> <a href="https://www.shapeai.tech" target="_blank"> ShapeAI </a> </h1>
+            <h3> Bootcamp Feedback </h3>
+        </header>
+    );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note() {
+    return (
+        <div className="note">
+            <h1> <a href="https://github.com/TharunNo1/" target="_blank"> ~Tharun G </a> </h1>
+            <p>Hi. This is Tharun G. Thanks for organizing this Web development bootcamp to enhance my knowledge in this field &#129303;.</p>
+            <p>Githublink for my project done on this bootcamp: <a href="https://github.com/TharunNo1/ShapeAI_Bootcamp_Web_Development" target="_blank">Click Here!</a></p>
+        </div>
+        );
+}
+
+export default Note;
